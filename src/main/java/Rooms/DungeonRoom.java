dfb7429224546a02fd33abdc19a677f236a6bd41
kerimsamba <108222522+kerimsamba@users.noreply.github.com>
2022-09-28 14:18:07 +0100

    Initial testing setup

--- src/main/java/Rooms/DungeonRoom.java
+++ src/main/java/Rooms/DungeonRoom.java
@@ -1,9 +1,6 @@
 package Rooms;
 
-import Objects.Enemies;
-import Objects.Potions;
-import Objects.Treasure;
-import Objects.Weapons;
+import Objects.*;
 import Players.PlayerClass;
 
 import java.util.ArrayList;
@@ -15,6 +12,7 @@ public class DungeonRoom {
     private ArrayList<Treasure> treasures;
     private ArrayList<Potions> potions;
     private ArrayList<Enemies> enemies;
+    private ArrayList<Familiars> familiars;
 
 
     public DungeonRoom(String name){
@@ -23,9 +21,55 @@ public class DungeonRoom {
         this.treasures = new ArrayList<Treasure>();
         this.potions = new ArrayList<Potions>();
         this.enemies = new ArrayList<Enemies>();
+        this.familiars = new ArrayList<Familiars>();
 
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public ArrayList<Weapons> getWeapons() {
+        return weapons;
+    }
+
+    public void addWeapon(Weapons weapon) {
+        this.weapons.add(weapon);
+    }
+
+    public ArrayList<Treasure> getTreasures() {
+        return treasures;
+    }
+
+    public void addTreasure(Treasure treasure) {
+        this.treasures.add(treasure);
+    }
+
+    public ArrayList<Potions> getPotions() {
+        return potions;
+    }
+
+    public void addPotion(Potions potion) {
+        this.potions.add(potion);
+    }
+
+    public ArrayList<Enemies> getEnemies() {
+        return enemies;
+    }
+
+    public void addEnemy(Enemies enemy) {
+        this.enemies.add(enemy);
+    }
+
+    public ArrayList<Familiars> getFamiliars() {
+        return familiars;
+    }
+
+    public void addFamiliar(Familiars familiar) {
+        this.familiars.add(familiar);
+    }
 }
